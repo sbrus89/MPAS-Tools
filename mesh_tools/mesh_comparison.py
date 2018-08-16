8cf4499621d028bba4ee2c1bae0881349349db57
Steven Brus <sbrus@lanl.gov>
2018-08-15 23:58:40 -0600

    Add save paths to mesh comparison.py
     - Save directory of the plots is now specifed instead of them
       being saved in the script execution directory
    
     - Add comments

--- mesh_tools/mesh_comparison.py
+++ mesh_tools/mesh_comparison.py
@@ -18,29 +18,37 @@ Entire_Globe = np.array([-180,180,-90,90])
 ##############################################################
 class comparison_plots:
 
-  def __init__(self,nmesh,var):
-    self.nmesh = nmesh
-    self.var = var
+  def __init__(self,nmesh,var,savepaths):
+
+      # Initialize varaiables common to all methods
+      self.nmesh = nmesh
+      self.var = var
+      self.savepaths = savepaths
 
   def plot_field(self,lon,lat,data,mesh_name,i,plot_box,bounds=''):
   
       print "   plotting field: " + self.var
       
+      # Get the data inside the plotting box
       lon_plot,lat_plot,data_plot = self.get_data_inside_box(lon,lat,data,plot_box)
   
+      # Initialize the figure and create subplot
       if i == 1:
         self.field = plt.figure()
       else:
         plt.figure(self.field.number)
       ax = self.field.add_subplot(nmesh,1,i)
-
+      
+      # Set the color scale bounds
       if bounds:
           levels = np.linspace(bounds[0],bounds[1],100)
       else:
           levels = np.linspace(np.amin(data_plot),np.amax(data_plot),100)
 
+      # Plot the data contours
       cplot = ax.tricontourf(lon_plot,lat_plot,data_plot,levels=levels)
 
+      # Plot the land, coastlines, and lat.lon lines
       m = Basemap(projection='cyl',llcrnrlat=plot_box[2],urcrnrlat=plot_box[3],\
                   llcrnrlon=plot_box[0],urcrnrlon=plot_box[1],resolution='c')    
       m.fillcontinents(color='grey',lake_color='white')
@@ -48,14 +56,19 @@ class comparison_plots:
       m.drawparallels(np.arange(plot_box[2],plot_box[3],20),labels=[True, False, False, False])
       m.drawmeridians(np.arange(plot_box[0],plot_box[1],60),labels=[False,False, False, True])
 
+      # Add the colorbar
       cb = self.field.colorbar(cplot,ax=ax)
       cb.set_label(self.var.lower())
-
+      
+      # Add axis and figure lables
       ax.set_title(mesh_name)
       ax.axis(plot_box)
       self.field.suptitle(self.var,y=1.05,fontweight='bold')
       self.field.tight_layout()
-      self.field.savefig(self.var.replace(' ','')+'_field.png',bbox_inches='tight')
+
+      # Save the figure
+      for path in self.savepaths:
+        self.field.savefig(path+self.var.replace(' ','')+'_field.png',bbox_inches='tight')
   
   ##############################################################
   
@@ -63,26 +76,33 @@ class comparison_plots:
   
       print "   plotting histogram: " + self.var
  
+      # Initialize the figure and create subplot
       if i == 1: 
         self.hist = plt.figure()
       else:
         plt.figure(self.hist.number)
       ax = self.hist.add_subplot(nmesh,1,i)
 
+      # Plot a histogram of the data
       ax.hist(data,'auto')
+
+      # Add axis and figure labels
       ax.set_title(mesh_name)
       ax.set_ylabel('count')
       ax.set_xlabel(self.var.lower())
+      self.hist.suptitle(self.var,y=1.05,fontweight='bold')
+      self.hist.tight_layout()
 
-
+      # Adjust the axis limits
       if bounds:
         ax.set_xlim([bounds[0],bounds[1]])
         ax.set_ylim([bounds[2],bounds[3]])
       else:
         self.make_axis_same(self.hist)
-      self.hist.suptitle(self.var,y=1.05,fontweight='bold')
-      self.hist.tight_layout()
-      self.hist.savefig(self.var.replace(' ','')+'_hist.png',bbox_inches='tight')
+
+      # Save the figure
+      for path in self.savepaths:
+        self.hist.savefig(path+self.var.replace(' ','')+'_hist.png',bbox_inches='tight')
 
   ##############################################################
   
@@ -90,15 +110,20 @@ class comparison_plots:
   
       print "   plotting lat average: " + self.var
   
+      # Initialize the latitude vectors
       lat_bins = np.arange(-90.0,90.0,binsize)
       lat_avg = np.zeros(lat_bins.shape)
       lat_min = np.zeros(lat_bins.shape)
       lat_max = np.zeros(lat_bins.shape)
-  
+ 
       for j in range(lat_bins.size-1):
           
+          # Find cells in this bin
           idx = np.where((lat > lat_bins[j]) & (lat <= lat_bins[j+1]))
           lat_data = data[idx] 
+
+          # Find average, minimum, and maximum of the data in this bin 
+          # (if no data is in the bin, set to NaN so it is ignored in the plot)
           if lat_data.size > 0:
               lat_avg[j] = np.mean(lat_data)
               lat_min[j] = np.min(lat_data)
@@ -108,25 +133,34 @@ class comparison_plots:
               lat_min[j] = np.nan
               lat_max[j] = np.nan
  
+      # Initialize the figure and add subplot 
       if i == 1: 
         self.latavg = plt.figure()
       else:
         plt.figure(self.latavg.number)
       ax = self.latavg.add_subplot(nmesh,1,i)
-
+ 
+      # Plot the average, minimum and maximum
       ax.fill_between(lat_bins,lat_min,lat_max,alpha=.5)
       ax.plot(lat_bins,lat_avg)
+
+      # Add axis and figure labels
       ax.set_title(mesh_name)
       ax.set_xlabel('latitude')
       ax.set_ylabel('average '+self.var.lower())
+      self.latavg.suptitle(self.var,y=1.05,fontweight='bold')
+      self.latavg.tight_layout()
+
+      # Adjust the axis limits
       if bounds:
         ax.set_xlim([bounds[0],bounds[1]])
         ax.set_ylim([bounds[2],bounds[3]])
       else:
         self.make_axis_same(self.latavg)
-      self.latavg.suptitle(self.var,y=1.05,fontweight='bold')
-      self.latavg.tight_layout()
-      self.latavg.savefig(self.var.replace(' ','')+'_lat.png',bbox_inches='tight')
+
+      # Save the figure
+      for path in self.savepaths:
+        self.latavg.savefig(path+self.var.replace(' ','')+'_lat.png',bbox_inches='tight')
   
   #############################################################
   
@@ -148,9 +182,11 @@ class comparison_plots:
 
   def make_axis_same(self,fig):
     
+    # Initialize the axis limits
     xlim_all = [1e99,-1e99]
     ylim_all = [1e99,-1e99]
 
+    # Find the lower and upper bounds of the subplot axes limits
     axes = fig.get_axes()
     for ax in axes:
       xlim = ax.get_xlim()
@@ -160,6 +196,7 @@ class comparison_plots:
       ylim_all[0] = min(ylim_all[0],ylim[0])
       ylim_all[1] = max(ylim_all[1],ylim[1])
 
+    # Set all axes limits to the lower and upper bounds
     for ax in axes:
       ax.set_xlim(xlim)
       ax.set_ylim(ylim)
@@ -169,11 +206,14 @@ class comparison_plots:
 ##############################################################
 
 meshes = [
-           #{'file':'/users/sbrus/scratch1/MPAS-O_testing/ocean/global_ocean/EC60to30/spin_up/mesh_metrics/mesh_with_metrics.nc',
+           #{'path':'/users/sbrus/scratch1/MPAS-O_testing/ocean/global_ocean/EC60to30/spin_up/mesh_metrics/',
+           # 'file':'mesh_with_metrics.nc',
            # 'name':'EC60to30V3'},
-           #{'file':'/users/sbrus/scratch1/MPAS-O_testing/ocean/global_ocean/EC60to30JS/init/mesh_metrics/mesh_with_metrics.nc',
+           #{'path':'/users/sbrus/scratch1/MPAS-O_testing/ocean/global_ocean/EC60to30JS/init/mesh_metrics/',
+           # 'file':'mesh_with_metrics.nc',
            # 'name':'EC60to30V4 (Jigsaw)'},
-           {'file':'/users/sbrus/scratch1/MPAS-O_testing/ocean/global_ocean/USNAEC60to30cr10/init/mesh_metrics/mesh_with_metrics.nc',
+           {'path':'/users/sbrus/scratch1/MPAS-O_testing/ocean/global_ocean/USNAEC60to30cr10/init/mesh_metrics/',
+            'file':'mesh_with_metrics.nc',
             'name':'USNAEC60to30cr10'}
          ]
 
@@ -182,16 +222,19 @@ plot_box = Entire_Globe
 ##############################################################
 # Main Program
 ##############################################################
+
+# Initialize comparison_plots class instances for each mesh metric
 nmesh = len(meshes)
-cell_size = comparison_plots(nmesh,'Cell Width')
-cell_qual = comparison_plots(nmesh,'Cell Quality')
+savepaths = [mesh['path'] for mesh in meshes]
+cell_size = comparison_plots(nmesh,'Cell Width',savepaths)
+cell_qual = comparison_plots(nmesh,'Cell Quality',savepaths)
 
 for i,mesh in enumerate(meshes):
    
     print "Mesh: " + mesh['name']
  
     # Open mesh and read in cell coordinates and data
-    ds = Dataset(mesh['file'],'r')
+    ds = Dataset(mesh['path']+mesh['file'],'r')
     loncell = (np.mod(ds.variables['lonCell'][:]+np.pi,2*np.pi)-np.pi)*rad2deg
     latcell = ds.variables['latCell'][:]*rad2deg
     areaCell = ds.variables['areaCell'][:]
